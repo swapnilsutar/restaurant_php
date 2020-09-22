4845a64ba44dadc4cea29d0455f49044d08179de
swapnil sutar <swapnilsutar902@gmail.com>
2020-09-22 23:33:09 +0530

    initial project

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->userType !== 'admin'){
+            return redirect()->route('home')->with('status','Write here your messege');
+        }
+        else{
+            return $next($request);
+        }
+
+
+
+    }
+}
