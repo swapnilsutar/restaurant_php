7fe1fb3d42bfd07ce4a01a2965d48ad263b391ad
swapnil sutar <swapnilsutar902@gmail.com>
2020-09-29 18:42:19 +0530

    Home page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,94 +4,105 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Indian Food Resto</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
-
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        
     </head>
     <body>
-        <div class="container">
+        <div class="">
             @if (Route::has('login'))
-                <div class="links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                @auth                      
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+            <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="#">Indian Food Restaurant </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">exp</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        </ul>
+                        
+                        <a class="nav-link" href="{{ url('/home') }}">Home </a>
+                        <a class="nav-link" href="#">Reservation</a>
+                        
+                        <a class="nav-link" href="#">menu</a>
+                        
+                        <a class="nav-link" href="#">Contact</a>
+                        
+                    <span class="navbar-text">
+                        
+                    <a href="{{ route('login') }}" class="nav-link">Login</a>
+                    </span>                
+                    @if (Route::has('register'))
+                    <span class="navbar-text">
+                    <a href="{{ route('register') }}" class="nav-link">Register</a>
+        
+                    </span>
                 </div>
-            @endif
+            </nav>
+         
+                        @endif
+                    @endauth  
+            @endif                     
+
 
             <div class="content">
-                <div class="title">
-                    Welcome to our Restaurant 
-                </div>
 
-                <h1>swapnil</h1>
+                <div class="bg_content">
 
-                <div class="links">
-                   
+                    <div class="c_title">
+                        Welcome to Indian Food Restaurant
+                    </div>
                 </div>
             </div>
+
+            <div class="reservation">
+
+            </div>
+
+            <div class="contact">
+
+
+            </div>
+
         </div>
-    </body>
+        
+        
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        
+<!-- 
+    <script>
+            $(document).ready(function() {
+  //parallax scroll
+            $(window).on("load scroll", function() {
+                var parallaxElement = $(".c_title"),
+                parallaxQuantity = parallaxElement.length;
+                window.requestAnimationFrame(function() {
+                for (var i = 0; i < parallaxQuantity; i++) {
+                    var currentElement = parallaxElement.eq(i),
+                    windowTop = $(window).scrollTop(),
+                    elementTop = currentElement.offset().top,
+                    elementHeight = currentElement.height(),
+                    viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+                    scrolled = windowTop - elementTop + viewPortHeight;
+                    currentElement.css({
+                    transform: "translate3d(0," + scrolled * -0.17 + "px, 0)"
+                    });
+                }
+                });
+            });
+            });
+
+
+    </script> -->
+    
+        </body>
+
 </html>
