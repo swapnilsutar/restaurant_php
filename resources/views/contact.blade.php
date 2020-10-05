98440a5cc3c10316d7d7a50710441b9d418bbeeb
swapnil sutar <swapnilsutar902@gmail.com>
2020-10-05 20:51:25 +0530

    contact page ready

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,89 @@
 
 
 @section('content')
-    <h1>Contact Page</h1>
+   
+<div class="contact_c">
+          <div class="insta_text">
+         get in touch
+                         <!-- contact indian food restaurant -->
+          </div>
+
+     </div>
+
+    <div class="form_time">
+    
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <center>
+                            
+                            <div class="con_tit">
+                                Opening times
+                        </div> 
+                        
+                        <div class="day">
+
+                            Monday – Tuesday
+                        </div>
+                        
+                        Dinner – 17:00 – 22:00
+                        
+                        <div class="day">
+
+                            Wednesday – Friday
+                        </div>
+                        
+                        Lunch- 12:00 – 14:00
+                        <br>
+                        Dinner – 17:00 – 22:00
+                        
+                        <div class="day">
+                            
+                            Saturday
+                            </div>
+                        
+                        Dinner – 16:00 – 23:00
+                        
+                        <div class="day">
+                            
+                            Sunday
+                            </div>
+                        
+                        Dinner – 14:00 – 22:00
+
+                    </center>
+                    </div>
+                    
+                    <div class="col-lg-4">
+                        <div class="con_tit">
+                                    contact us
+                            </div> 
+
+                            <form action="mailto:swapnilsutar@gmail.com" method="post" enctype="text/plain">
+
+                            <input type="text" class="form-control" placeholder="Your Name" >
+<br>
+                            <input type="email" class="form-control" placeholder="Your email address" required>
+<br>
+                            
+                            <input type="text" class="form-control" placeholder="Subject" required>
+                            <br>
+
+                            <textarea name="msg" id="msg" class="form-control" cols="30" rows="5" placeholder="Your Message" required></textarea>
+                            <br>
+
+                            <input type="submit" value="Submit" class="btn btn-success btn-block" >
+                            </form>
+
+                    </div>
+
+
+                
+                </div>
+            </div>
+
+    </div>
+
 
 @endsection
--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -17,7 +17,7 @@
      <div class="main_reserve">
           <div class="container">
           <center>
-
+          
                <div class="reserv_title">
                     MAKE A RESERVATION
                </div>               
@@ -25,7 +25,57 @@
                <button class="reserve_btn">
                     find a table
                </button>
+
           </center>
+<br>
+          <div class="row">
+               <div class="col-lg-6">
+                    <div class="rt">
+                    by phone
+                    </div>
+
+                    <div class="rt_t">
+                         Prefer to make your reservation via phone?
+                    </div>
+
+               </div>
+               <div class="col-lg-6">
+                    <div class="rt">
+                         special events & parties
+                    </div>
+                    <div class="rt_t">
+                    Catering and Events are opportunities for us to work more closely with our guests, occasions for us to share our dedication to celebration in exciting new ways.
+                    </div>
+               </div>
+               
+          </div>
+
+          <div class="row">
+               <img src="/images/Select-Restaurants-In-India-Offer-Up-To-50-Discount.jpg" alt="" class="rt_img" >
+          </div>
+
+          <div class="row">
+               <div class="col-lg-6">
+                    <div class="rt">
+                    THE BIG ROOM
+                    </div>
+
+                    <div class="rt_t">
+                         Seats up to 80
+                    </div>
+
+               </div>
+               <div class="col-lg-6">
+                    <div class="rt">
+                         the small room
+                    </div>
+                    <div class="rt_t">
+                    Seats up to 25
+                    </div>
+               </div>
+               
+          </div>
+
           </div>
      </div>
 
